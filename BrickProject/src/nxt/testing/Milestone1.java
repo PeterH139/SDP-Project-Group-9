d7ee9fbae4b2b491f00d536cacba5b2f4c185677
Ross Grassie <rossgrassie@hotmail.co.uk>
2014-01-20 16:30:29 +0000

    Changed Milestone 1 to use incremental direction changes.

--- BrickProject/src/nxt/testing/Milestone1.java
+++ BrickProject/src/nxt/testing/Milestone1.java
@@ -8,86 +8,114 @@ import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.Sound;
 import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.navigation.Pose;
+import lejos.geom.*;
  
 
 public class Milestone1 {
 	
-	static final int TYRE_DIAMETER = 56;
-	static final int TRACK_WIDTH = 110;
-	
-	private static int THRESHOLD_VALUE;
+	private static Pose brick = new Pose();
+	private static Point start;
+	private static boolean first = true;
 	
 	private enum Location {
-		UNKNOWN, FACING_WALL, PARALLEL_TO_WALL
+		UNKNOWN, ON_EDGE, PARALLEL_TO_WALL
 	}
-
+	
+	static final int TYRE_DIAMETER = 56;
+	static final int TRACK_WIDTH = 120;
+	static final double ROTATE_ANGLE = 2;
 	static NXTRegulatedMotor leftMotor = Motor.A;
 	static NXTRegulatedMotor rightMotor = Motor.B;
-	
 	static DifferentialPilot pilot = new DifferentialPilot(TYRE_DIAMETER, TRACK_WIDTH, leftMotor, rightMotor);
 	
+	static int thresholdValue;
 	static LightSensor lsLeft = new LightSensor(SensorPort.S1);
 	static LightSensor lsRight = new LightSensor(SensorPort.S2);
-	
 	static Location loc = Location.UNKNOWN;
-	private static boolean die = false;
+	static boolean die = false;
+	static int lowLightValue, highLightValue;
+	static boolean isWhiteRight, isWhiteLeft, haveMadeContact, rightHitFirst;
 	
-	private static int lowLightValue, highLightValue;
-	private static boolean isWhiteRight, isWhiteLeft;
 	
 	public static void main(String[] args) {
 		leftMotor.setSpeed(leftMotor.getMaxSpeed()/20);
 		rightMotor.setSpeed(rightMotor.getMaxSpeed()/20);
 		calibrateValues();
+		Point origin = brick.getLocation();
 		while(!die){
 			// Check Sensor and button inputs
 			int rightReading = lsRight.getLightValue();
 			int leftReading = lsLeft.getLightValue();
-			isWhiteRight = (rightReading > THRESHOLD_VALUE) ? true : false;
-			isWhiteLeft = (leftReading > THRESHOLD_VALUE) ? true : false;
+			isWhiteRight = (rightReading > thresholdValue) ? true : false;
+			isWhiteLeft = (leftReading > thresholdValue) ? true : false;
 			
 			if (Button.LEFT.isDown()){
 				calibrateValues();
+				loc = Location.UNKNOWN;
+				LCD.clear();
+				LCD.drawString("I'm Lost! :(", 0, 2);
 			} else if (Button.ESCAPE.isDown()){
 				die = true;
 			}
 
 			// Determine appropriate action, based upon location and sensor inputs.
+			// Carry out action and update location.
 			switch (loc){
 			case UNKNOWN:
-				if (isWhiteRight) rightMotor.flt(); else rightMotor.forward();
-				if (isWhiteLeft) leftMotor.flt(); else leftMotor.forward();
-				if (isWhiteRight && isWhiteLeft){
-					loc = Location.FACING_WALL;
-					LCD.clear();
-					LCD.drawString("Facing a wall!", 0, 2);
+				if (first) start = brick.getLocation(); first = false;
+				if (!haveMadeContact){
+					pilot.forward();
+					if (isWhiteRight || isWhiteLeft){
+						rightHitFirst = isWhiteRight ? true : false;
+					}
+				} else {
+					if (isWhiteRight && isWhiteLeft){
+						loc = Location.ON_EDGE;
+						LCD.clear();
+						LCD.drawString("On Edge!", 0, 2);
+					}
 				}
 				break;
-			case FACING_WALL:
-				if (isWhiteRight && isWhiteLeft){
-					pilot.travel(-50);
-					pilot.rotate(90);
-					pilot.forward();
-					loc = Location.PARALLEL_TO_WALL;
-					LCD.clear();
-					LCD.drawString("Paralell to wall!", 0, 2);
-				} else {
-					loc = Location.UNKNOWN;
+			case ON_EDGE:
+				if (first) start = brick.getLocation(); first = false;
+				while (isWhiteRight && isWhiteLeft){
+					if (rightHitFirst){
+						pilot.rotate(ROTATE_ANGLE);
+					} else {
+						pilot.rotate(-ROTATE_ANGLE);
+					}
 				}
+				pilot.forward();
+				loc = Location.PARALLEL_TO_WALL;
+				LCD.clear();
+				LCD.drawString("Paralell to wall!", 0, 2);
+				checkIfLost();
 				break;
 			case PARALLEL_TO_WALL:
+				if (first) start = brick.getLocation(); first = false;
 				if (isWhiteRight && isWhiteLeft){
 					pilot.stop();
-					loc = Location.FACING_WALL;
+					loc = Location.ON_EDGE;
 					LCD.clear();
-					LCD.drawString("Facing a wall!", 0, 2);
-				} else if (isWhiteRight ^ isWhiteLeft){
-					pilot.forward();
-				} else {
-					loc = Location.UNKNOWN;
-				}
+					LCD.drawString("On Edge!", 0, 2);
+				} 
+				checkIfLost();
 			}
-			// Carry out action and update location.
+			if (brick.getLocation() == start) {
+				float bearing = brick.relativeBearing(origin);
+				brick.rotateUpdate(bearing);
+				float distance = brick.distanceTo(origin);
+				brick.moveUpdate(distance);
+			}
+		}
+	}
+
+	private static void checkIfLost() {
+		if (!(isWhiteRight || isWhiteLeft)){
+			loc = Location.UNKNOWN;
+			LCD.clear();
+			LCD.drawString("I'm Lost! :(", 0, 2);
 		}
 	}
 
@@ -108,6 +136,6 @@ public class Milestone1 {
 		highLightValue = lsLeft.getLightValue();        
 		Sound.playTone(1000, 200, 100);
 		
-		THRESHOLD_VALUE = (highLightValue + lowLightValue)/2;
+		thresholdValue = (highLightValue + lowLightValue)/2;
 	}
 }
